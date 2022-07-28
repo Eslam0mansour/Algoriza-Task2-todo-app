e2b1109b94bd92e270528166bdf64c5329f9f58d
Eslam0mansour <eslam55855@gmail.com>
2022-07-28 15:11:48 +0200

    happy

--- lib/shared/components/item_task.dart
+++ lib/shared/components/item_task.dart
@@ -15,7 +15,7 @@ class ItemTask extends StatelessWidget {
 
     return  Container(
           width: double.infinity,
-          height: 90,
+          height: 100,
           margin: const EdgeInsets.all(10),
           padding: const EdgeInsets.all(15),
           decoration: BoxDecoration(
@@ -28,7 +28,7 @@ class ItemTask extends StatelessWidget {
               Column(
                 children:  [
                   Text(
-                    '${task['title']}',
+                    '${task['start']}',
                     style:TextStyle(
                         color: Colors.white,
                         fontWeight: FontWeight.bold
@@ -38,7 +38,7 @@ class ItemTask extends StatelessWidget {
                     height: 7,
                   ),
                   Text(
-                    '${task['date']}',
+                    '${task['title']}',
                     style:TextStyle(
                         color: Colors.white,
                         fontWeight: FontWeight.bold
@@ -46,34 +46,45 @@ class ItemTask extends StatelessWidget {
                   ),
                 ],
               ),
-              Row(
+              Column(
                 children: [
-                  IconButton(
-                      onPressed: (){
-                        AppCubit.get(context).update(status: 'done', id:  task['id'] );
-                      },
-                      icon: SvgPicture.asset(
-                          "assets/a.svg",
+                  Text(
+                    '${task['date']}',
+                    style:TextStyle(
                         color: Colors.white,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                          onPressed: (){
+                            AppCubit.get(context).update(status: 'done', id:  task['id'] );
+                          },
+                          icon: SvgPicture.asset(
+                              "assets/a.svg",
+                            color: Colors.white,
+                          ),
                       ),
-                  ),
-                  IconButton(
-                    onPressed: (){
-                      AppCubit.get(context).update(status: 'fav', id:  task['id'] );
+                      IconButton(
+                        onPressed: (){
+                          AppCubit.get(context).update(status: 'fav', id:  task['id'] );
 
-                    },
-                    icon: Icon(
-                      Icons.favorite_border
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: (){
-                      AppCubit.get(context).deletitem(id: task['id']);
-                    },
-                    icon: Icon(
-                      Icons.delete_forever_outlined,
-                      color: Colors.grey[600],
-                    ),
+                        },
+                        icon: Icon(
+                          Icons.favorite_border
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: (){
+                          AppCubit.get(context).deletitem(id: task['id']);
+                        },
+                        icon: Icon(
+                          Icons.delete_forever_outlined,
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
